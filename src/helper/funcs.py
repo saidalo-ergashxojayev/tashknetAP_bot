0ce98706d103f37ff3a0a85459106e94f2835de8
Azizbek Qodirov <qodirovazizbek249@gmail.com>
2025-04-18 03:15:06 +0500

    gitignore changed

--- src/helper/funcs.py
+++ src/helper/funcs.py
@@ -17,7 +17,7 @@ def create_start_text_text(short_link: str) -> StartTextResult:
         "- <b>Practice</b> uchun qo'shimcha 9 ta dars ✅\n" +
         "- 4 ta ustozdan <b>'pichoqqa sop bo'ladigan'</b> maslahatlar ✅\n" +
         "- Samarali texnikalar ✅\n" +
-        "- <b>20 kunlik </b> ✅\n\n" +
+        "- <b>20 kunlik to'liq nazorat</b> ✅\n\n" +
         "Barchasi mutlaqo <b>TEKINGA!</b>\n\n" +
         "Bu kursning boshqa kurslardan farqi sizga faqat test emas, barcha Reading 9.0 uchun ko'nikmalar ipidan ignasigacha tushuntiriladi. 🚀\n\n" +
         "Pastdagi havola orqali ro'yxatdan o'ting va marafonda bepul qatnashing 👇🏻\n" +
